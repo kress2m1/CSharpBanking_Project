db678314fa3594f7fc34cc8ae690fa883d5130c2
Andre Dada <kress2m1@gmail.com>
2016-07-14 13:03:29 +0100

    Annotations have been added to the project

---
+++ UIAutomationSoln/UIAutomation/WebTest/HomeLoadCheck.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UIAutomation.WebTest
+{
+    [TestClass]
+    public class HomeLoadCheck
+    {
+        //Initialise the instance of the Iwebdriver
+        private IWebDriver _driver;
+
+        /// <summary>
+        /// These tasks must be run before the tests commence
+        /// </summary>
+        [TestInitialize]
+        public void PreCondition()
+        {
+            Console.WriteLine("These tasks must be run before the tests begin");
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+        }
+
+        [TestMethod, TestCategory("Home Page Test")]
+        public void CheckHomePageIsLoaded()
+        {
+            Console.WriteLine("Check that the browser is loaded");
+        }
+
+        [TestMethod, TestCategory("Home Page Test"), Ignore]
+        public void CheckBrowserIsMaximised()
+        {
+            Console.WriteLine("Check that the browser is maximised");
+        }
+
+        /// <summary>
+        /// These tests must be run after the tests have been run
+        /// </summary>
+        [TestCleanup]
+        public void PostCondition()
+        {
+            Console.WriteLine("These tasks must be run after the tests have been run");
+            _driver.Quit(); 
+        }
+    }
+}
---
+++ UIAutomationSoln/UIAutomation/WebTest/LoginTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UIAutomation.WebTest
+{
+    [TestClass]
+    public class LoginTest
+    {
+        private IWebDriver _driver;
+
+        /// <summary>
+        /// These tasks must be run before the tests start
+        /// </summary>
+        [TestInitialize]
+        public void PreCondition()
+        {
+            Console.WriteLine("These tasks must be run before the tests are run");
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();    
+        }
+
+        [TestMethod]
+        [Owner("Andre Dada")]
+        [TestProperty("Browser", "Launch")]
+        [TestCategory("SmokeTest")]
+        public void LaunchBrowser()
+        {
+            Console.WriteLine("Check that the browser is launched and maximised");
+        }
+         
+        [TestMethod]
+        [Owner("Andre Dada")]
+        [TestProperty("Login", "Username")]
+        [TestCategory("SmokeTest")]
+        public void LoginToTescoAccount()
+        {
+            Console.WriteLine("Check that the user is able to log into their account");
+        }
+
+        /// <summary>
+        /// These tasks must be run after the tests are completed
+        /// </summary>
+        [TestCleanup]
+        public void PostCondition()
+        {
+            Console.WriteLine("These tasks must be run after the tests runs are completed");
+            _driver.Quit();
+        }
+
+    }
+}
