338770db31c37a40f5c79421de842c5d6ccbd95e
Andre Dada <kress2m1@gmail.com>
2016-07-25 12:27:19 +0100

    Added updates to Solution 25-07-2016

--- UIAutomationSoln/UIAutomation/WebTest/HomeLoadCheck.cs
+++ UIAutomationSoln/UIAutomation/WebTest/HomeLoadCheck.cs
@@ -24,15 +24,51 @@ namespace UIAutomation.WebTest
             Console.WriteLine("These tasks must be run before the tests begin");
             _driver = new ChromeDriver();
             _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl("http://www.asda.com");
+
+            Console.WriteLine("Display the list of cookies on the website");
+            IList<Cookie> cookies = _driver.Manage().Cookies.AllCookies;
+            foreach (var cookie in cookies)
+            {
+                Console.WriteLine("The list of cookies {0} \n", cookie.ToString());
+            }
         }
 
-        [TestMethod, TestCategory("Home Page Test")]
+        [TestMethod]
+        [Owner("Andre Dada")]
+        [TestCategory("Home Page Test")]
+        public void CheckPageUrl()
+        {
+            string urlCheck = _driver.Url;
+            Console.WriteLine(urlCheck);
+            bool asserCheckUrl = _driver.Url.ToLower().Contains("www.asda.com".ToLower());
+            Assert.IsTrue(asserCheckUrl, "Does not contain {0}", "www.asda.com.".ToLower());
+        }
+
+
+        [TestMethod]
+        [Owner("Andre Dada")]
+        [TestCategory("Home Page Test")]
         public void CheckHomePageIsLoaded()
         {
-            Console.WriteLine("Check that the browser is loaded");
+            Console.WriteLine("Check that the homepage is loaded");
+            //_driver.FindElement(By.ClassName("section__title"));
+            bool assertCheck = _driver.PageSource.ToLower().Contains("Explore more at Asda".ToLower());
+            Assert.IsTrue(assertCheck, "Does not contain {0}", "Explore more at Asda");
         }
 
-        [TestMethod, TestCategory("Home Page Test"), Ignore]
+        [TestMethod]
+        [Owner("Andre Dada")]
+        [TestCategory("Home Page Test")]
+        public void CheckPageTitle()
+        {
+            string asdaTitle = _driver.Title;
+            Assert.AreEqual("Asda.com - Online Food Shopping, George, & more".ToLower(), asdaTitle.ToLower());
+        }
+
+        [TestMethod]
+        [Owner("Andre Dada")]
+        [TestCategory("Home Page Test")]
         public void CheckBrowserIsMaximised()
         {
             Console.WriteLine("Check that the browser is maximised");
@@ -43,7 +79,7 @@ namespace UIAutomation.WebTest
         /// </summary>
         [TestCleanup]
         public void PostCondition()
-        {
+        {  
             Console.WriteLine("These tasks must be run after the tests have been run");
             _driver.Quit(); 
         }
